b8f470ce9e011a4b90ca1100f578ea04019f29ac
Lorenzo Pollastrini <lorenzo.pollastrini@outlook.com>
2022-05-20 15:00:30 +0200

    Creato progetto di partenza

---
+++ src/main/java/it/mantik/esquid/EsquidApplication.java
@@ -0,0 +1,13 @@
+package it.mantik.esquid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsquidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsquidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/mantik/esquid/EsquidApplicationTests.java
@@ -0,0 +1,13 @@
+package it.mantik.esquid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsquidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

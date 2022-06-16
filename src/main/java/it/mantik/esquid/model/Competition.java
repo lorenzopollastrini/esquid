128d3cf6b6686dbfd0680b612eaf9ac892c1507a
Lorenzo Pollastrini <lorenzo.pollastrini@outlook.com>
2022-06-16 10:23:14 +0200

    Cambiato alcune validazioni e i messaggi di validazione

--- src/main/java/it/mantik/esquid/model/Competition.java
+++ src/main/java/it/mantik/esquid/model/Competition.java
@@ -11,7 +11,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -36,12 +35,10 @@ public class Competition {
 	private Collection<PlayedMatch> playedMatches;
 	
 	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
-	@Future
 	@NotNull
 	private LocalDateTime startDateTime;
 
 	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
-	@Future
 	@NotNull
 	private LocalDateTime endDateTime;
 	

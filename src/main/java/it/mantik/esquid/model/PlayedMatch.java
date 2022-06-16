474ce4a33c1c4fbff432859b24310d602a38c83a
Lorenzo Pollastrini <lorenzo.pollastrini@outlook.com>
2022-06-16 12:45:34 +0200

    Rimosse alcune classi e validazioni non necessarie

--- src/main/java/it/mantik/esquid/model/PlayedMatch.java
+++ src/main/java/it/mantik/esquid/model/PlayedMatch.java
@@ -10,7 +10,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -31,7 +30,6 @@ public class PlayedMatch {
 	private Competition competition;
 	
 	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
-	@Future
 	@NotNull
 	private LocalDateTime dateTime;
 	
